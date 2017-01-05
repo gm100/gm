9ae6ead496a68428d55a1913cf7a65dde769deb0
sunyl <40603993@qq.com>
2017-01-05 15:04:30 +0800

    haishen1

---
+++ src/main/java/com/demo/test/test1.java
@@ -0,0 +1,15 @@
+package com.demo.test;
+
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2016/12/27 0027.
+ */
+public class test1 {
+    public static void main(String[] args) {
+        Date da =new Date();
+
+    }
+
+
+}
